7424f3fa4341d325160d09a10d770de998700e20
Z <asterixandisaac@gmail.com>
2020-12-23 13:47:45 -0700

    add shadow state

--- components/interactive/hoverable.tsx
+++ components/interactive/hoverable.tsx
@@ -9,7 +9,7 @@ const Hoverable: React.FC<hoverableProps> = ({ children }) => {
       whileHover={{
         y: -5,
         scale: 1.005,
-        shadow: `rgba(149, 157, 165, 0.2) 0px 8px 24px;`,
+        boxShadow: `10px 10px 0 rgba(0, 0, 0, 0.2)`,
       }}
     >
       {children}
